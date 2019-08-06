ed68ec2ee5aabd9eeb856d702ca291b1746d89cb
mohamedsaid <mcs.cadde@gmail.com>
2019-08-06 10:52:50 -0700

    Adding NAH-Beer controller #3

--- src/main/java/mas/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/mas/springframework/msscbeerservice/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+// Impl
 
 @RequestMapping("/api/v1/beer")
 @RestController
